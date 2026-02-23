18a0c83b7ff897e2413ece7dbb04433e1440e5fe
harsheeen <harsheenkaur023@gmail.com>
2026-02-23 13:28:40 +0530

    committed main function

---
+++ GuessingApp.java
@@ -0,0 +1,13 @@
+/** 
+ * Guessing app - use case 1 : game initilaistion
+ * application entry point
+ * no user entry
+ */
+
+public class GuessingApp{
+    public static void main(String[] args) {
+    System.out.println("welcome to guessing game");
+    GameConfig gameconfig =new GameConfig();
+    game.showrules();
+    }
+}
