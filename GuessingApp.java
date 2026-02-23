c0fb128d8e7096baf7f8b39ec82c44fd2ae57d6a
harsheeen <harsheenkaur023@gmail.com>
2026-02-23 14:37:11 +0530

    Update GuessingApp.java

--- GuessingApp.java
+++ GuessingApp.java
@@ -2,12 +2,14 @@
  * Guessing app - use case 1 : game initilaistion
  * application entry point
  * no user entry
+ @author Harsheen
+ @version 1.0
  */
 
 public class GuessingApp{
     public static void main(String[] args) {
     System.out.println("welcome to guessing game");
     GameConfig gameconfig =new GameConfig();
-    game.showrules();
+    gameconfig.showrules();
     }
 }
