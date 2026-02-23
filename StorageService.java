01c73d87b26de6211d180dbe2defb45a019d03f2
harsheeen <harsheenkaur023@gmail.com>
2026-02-23 16:29:16 +0530

    function to print details in txt file

---
+++ StorageService.java
@@ -0,0 +1,23 @@
+// function to create a text file and store the results
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class StorageService{
+
+public static void saveResult(String player, int attempts, boolean WIN){
+try(
+    BufferedWriter writer= new BufferedWriter(new FileWriter("game_results.txt"))){
+    writer.write("player:"  + player + ",\t attempts"+attempts+ "\t results"+(WIN?"Win" :"lose"));
+    writer.newLine();
+
+
+    }
+catch(IOException e){
+    System.out.println("unable to print game result");
+}
+
+}
+
+}
