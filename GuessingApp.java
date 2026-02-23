c401d1df2737495bd25e9e6ccfe4353130906f35
harsheeen <harsheenkaur023@gmail.com>
2026-02-23 15:04:05 +0530

    Update GuessingApp.java

--- GuessingApp.java
+++ GuessingApp.java
@@ -1,28 +1,38 @@
 /** 
  * Guessing app - use case 4 : game initilaistion + user input + hint + validateinput
  * application entry point
- * no user entry
+@author Harsheen
+@version 4.0
  */
 
 public class GuessingApp{
     public static void main(){
-    Scanner sc=new Scanner(System.in);
-    System.out.println("welcome to guessing game");
-    GuessingApp game=new GuessingApp();
-    game.showrules();
-    int attempts=0;
-    int hint=0;
-    while(attempts<game.MAX_ATTEMPTS){
-    System.out.print("enter the guess\t");
-    attempts++;
-    hint++;
-    int guess=sc.nextInt();
-    guess=ValidateInput.validainput(guess);
-    int valid= game.validateguess(guess,game.targetnumber);
-    if(valid==1){
-        break;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("welcome to guessing game");
+        
+        GuessingApp game=new GuessingApp();
+        
+        //display the rules
+        GameConfig.showrules();
+        
+        int attempts=0;
+        int hint=0;
+        //loop for user input till it reaches max attempts or user guess the correct input 
+        while(attempts<game.MAX_ATTEMPTS){
+            System.out.print("enter the guess\t");
+            
+            attempts++; //increment the user attempts
+            hint++; //increment the user hint 
+            
+            int guess=sc.nextInt(); // take the user input
+            
+            guess=ValidateInput.validainput(guess); //validate the user input 
+            int valid= GuessValidator.validateguess(guess,game.targetnumber); //valid the guess by displaying "go high" or "go low" or return 1 if the guess is right
+            
+            if(valid==1){ //user guess correct target
+                break;
     }
-    System.out.println(game.generatehint(hint));
+    System.out.println(HintGenerator.generatehint(hint));
     }
 }
 }
