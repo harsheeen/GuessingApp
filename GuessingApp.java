c892b0a255f3666531dce6c6e6f37a5c83205b30
harsheeen <harsheenkaur023@gmail.com>
2026-02-23 13:46:41 +0530

    main function commited

--- GuessingApp.java
+++ GuessingApp.java
@@ -5,9 +5,20 @@
  */
 
 public class GuessingApp{
-    public static void main(String[] args) {
+    public static void main(){
+    Scanner sc=new Scanner(System.in);
     System.out.println("welcome to guessing game");
-    GameConfig gameconfig =new GameConfig();
+    GuessingApp game=new GuessingApp();
     game.showrules();
+    int attempts=0;
+    while(attempts<game.MAX_ATTEMPTS){
+    System.out.print("enter the guess\t");
+    attempts++;
+    int guess=sc.nextInt();
+    int valid= game.validateguess(guess,game.targetnumber);
+    if(valid==1){
+        break;
     }
+    }
+}
 }
