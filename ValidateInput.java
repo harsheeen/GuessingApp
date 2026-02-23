b0698571f42a8710680b86a67055d2adca6e8089
harsheeen <harsheenkaur023@gmail.com>
2026-02-23 14:54:04 +0530

    Update ValidateInput.java

--- ValidateInput.java
+++ ValidateInput.java
@@ -1,11 +1,11 @@
 class ValidateInput extends Exception {
-  
+  //define the user defined exception function
     public ValidateInput(String message) {
         super(message); 
         System.out.println("UserDefinedException created: " + message);
     }
 
-    
+    //function to validate the user input using user defined exception - validainput
     public static int validainput(String input) throws ValidateInput {
         int value = Integer.parseInt(input);
         if (value < 1 || value > 100) {
