192df116d3fa7eead7155885a25023534666d53f
harsheeen <harsheenkaur023@gmail.com>
2026-02-23 14:43:50 +0530

    Update GameConfig.java

--- GameConfig.java
+++ GameConfig.java
@@ -29,7 +29,7 @@ public int getMaxhints(){
     return MAX_HINTS;
 }
 //display the rules of the game
-public void showrules(){
+public static void showrules(){
     System.out.println("guess a number between " +MIN+ "and"+MAX);
     System.out.println("You have"+MAX_ATTEMPTS+"attempts");
     System.out.println("Hints will be provided after wrong answers");
