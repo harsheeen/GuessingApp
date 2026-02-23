217eb44b863cf8bebd1866bc7e8e411f4f20f037
harsheeen <harsheenkaur023@gmail.com>
2026-02-23 14:48:29 +0530

    Update GuessValidator.java

--- GuessValidator.java
+++ GuessValidator.java
@@ -1,15 +1,16 @@
+//function to validate the user guess
 class GuessValidator{
-public int validateguess(int guess, int target){
-    if(target==guess){
+public static int validateguess(int guess, int target){
+    if(target==guess){ //return 1 if the user input is same as target
         System.out.println("correct");
         return 1;
     }
-    else if(target>guess){
-        System.out.println("HIGH");
+    else if(target>guess){ //return string go high if the user input is less than target
+        System.out.println(" GO HIGH");
         return 0;
     }
-    else if(target<guess){
-        System.out.println("LOW");
+    else if(target<guess){ ////return string go low if the user input is more than target
+        System.out.println("GO LOW");
         return 0;
     }
 return 0;
