d9dd73b865454069cab15151a876d0a15c5204e1
harsheeen <harsheenkaur023@gmail.com>
2026-02-23 14:46:22 +0530

    Update GuessingApp.java

--- GuessingApp.java
+++ GuessingApp.java
@@ -1,23 +1,29 @@
 /** 
- * Guessing app - use case 1 : game initilaistion
+ * Guessing app - use case 2 : game initilaistion and the user input 
  * application entry point
- * no user entry
+ @author Harsheen
+ @version 2.0
  */
 
 public class GuessingApp{
     public static void main(){
-    Scanner sc=new Scanner(System.in);
-    System.out.println("welcome to guessing game");
-    GuessingApp game=new GuessingApp();
-    game.showrules();
-    int attempts=0;
-    while(attempts<game.MAX_ATTEMPTS){
-    System.out.print("enter the guess\t");
-    attempts++;
-    int guess=sc.nextInt();
-    int valid= game.validateguess(guess,game.targetnumber);
-    if(valid==1){
-        break;
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("welcome to guessing game");
+        GuessingApp game=new GuessingApp();
+        GameConfig.showrules();
+        
+        int attempts=0;
+        //loop for user input until he reaches max attempts
+        while(attempts<GameConfig.MAX_ATTEMPTS){
+            System.out.print("enter the guess\t");
+            attempts++;
+            int guess=sc.nextInt();
+            //validate if the user guess is between 1 and 100
+            int valid= game.validateguess(guess,game.targetnumber);
+            //exit the loop if the user entry is right
+            if(valid==1){
+                break;
     }
     }
 }
