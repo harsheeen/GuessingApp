12e80ed47154c113552bcbe8851386fbc14aee76
harsheeen <harsheenkaur023@gmail.com>
2026-02-25 12:03:30 +0530

    Add: Game Initialization as Usecase 1

--- GameConfig.java
+++ GameConfig.java
@@ -30,7 +30,7 @@ public int getMaxhints(){
 }
 //display the rules of the game
 public void showrules(){
-    System.out.println("guess a number between " +MIN+ "and"+MAX);
+    System.out.println("guess a number between " +MIN+ "&"+MAX);
     System.out.println("You have"+MAX_ATTEMPTS+"attempts");
     System.out.println("Hints will be provided after wrong answers");
 
